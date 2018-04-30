5d05b6d7f9c92bb6db7301a1ad7cbd1f1d1fa1a9
neil.middleton <neil.middleton@thomsonreuters.com>
2018-04-30 14:20:50 +0100

    path to timetrigger http handler

--- TimerTriggerJS1/lib/index.js
+++ TimerTriggerJS1/lib/index.js
@@ -1,5 +1,2 @@
-module.exports.bluebird  = require('./bluebird');
-module.exports['request-promise'] = require('./request-promise');
-module.exports['request-promise-core'] = require('./request-promise-core');
-module.exports['stealthy-require'] = require('./stealthy-require');
-module.exports['tough-cookie'] = require('./tough-cookie');
+module.exports.http_request  = require('./http_request');
+module.exports.https_request  = require('./https_request');
